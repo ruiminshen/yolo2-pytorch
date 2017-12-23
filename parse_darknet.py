68e99ac88cb6d1a653eabd945eaeb58b084e49d1
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2017-12-23 15:30:18 +0800

    修正bug，支持部分读取.weights文件

--- parse_darknet.py
+++ parse_darknet.py
@@ -74,7 +74,7 @@ def main():
         utils.modify_config(config, cmd)
     if args.level:
         logging.getLogger().setLevel(args.level.upper())
-    cache_dir = utils.get_cache_dir()
+    cache_dir = utils.get_cache_dir(config)
     model_dir = utils.get_model_dir(config)
     category = utils.get_category(config, cache_dir if os.path.exists(cache_dir) else None)
     anchors = utils.get_anchors(config)
@@ -84,37 +84,40 @@ def main():
     logging.info(humanize.naturalsize(sum(var.cpu().numpy().nbytes for var in dnn.state_dict().values())))
     state_dict = dnn.state_dict()
     grouped_dict = group_state(state_dict)
-    layers = []
-    with open(os.path.expanduser(os.path.expandvars(args.file)), 'rb') as f:
-        major, minor, revision, seen = struct.unpack('4i', f.read(16))
-        logging.info('major=%d, minor=%d, revision=%d, seen=%d' % (major, minor, revision, seen))
-        total = 0
-        for layer in grouped_dict:
-            group = grouped_dict[layer]
-            for suffix in ['conv.bias', 'bn.bias', 'bn.weight', 'bn.running_mean', 'bn.running_var', 'conv.weight']:
-                if suffix in group:
-                    var = group[suffix]
-                    size = var.size()
-                    cnt = np.multiply.reduce(size)
-                    total += cnt
-                    key = layer + '.' + suffix
-                    val = np.array(struct.unpack('%df' % cnt, f.read(cnt * 4)), np.float32)
-                    val = np.reshape(val, size)
-                    logging.info('%s.%s: %s=%f (%s)' % (layer, suffix, 'x'.join(list(map(str, size))), utils.abs_mean(val), hashlib.md5(val.tostring()).hexdigest()))
-                    layers.append([key, torch.from_numpy(val)])
-            logging.info('%d parameters assigned' % total)
-        remaining = os.fstat(f.fileno()).st_size - f.tell()
+    try:
+        layers = []
+        with open(os.path.expanduser(os.path.expandvars(args.file)), 'rb') as f:
+            major, minor, revision, seen = struct.unpack('4i', f.read(16))
+            logging.info('major=%d, minor=%d, revision=%d, seen=%d' % (major, minor, revision, seen))
+            total = 0
+            filesize = os.fstat(f.fileno()).st_size
+            for layer in grouped_dict:
+                group = grouped_dict[layer]
+                for suffix in ['conv.bias', 'bn.bias', 'bn.weight', 'bn.running_mean', 'bn.running_var', 'conv.weight']:
+                    if suffix in group:
+                        var = group[suffix]
+                        size = var.size()
+                        cnt = np.multiply.reduce(size)
+                        total += cnt
+                        key = layer + '.' + suffix
+                        val = np.array(struct.unpack('%df' % cnt, f.read(cnt * 4)), np.float32)
+                        val = np.reshape(val, size)
+                        remaining = filesize - f.tell()
+                        logging.info('%s.%s: %s=%f (%s), remaining=%d' % (layer, suffix, 'x'.join(list(map(str, size))), utils.abs_mean(val), hashlib.md5(val.tostring()).hexdigest(), remaining))
+                        layers.append([key, torch.from_numpy(val)])
+                logging.info('%d parameters assigned' % total)
+        layers[-1][1] = transpose_weight(layers[-1][1], len(anchors))
+        layers[-2][1] = transpose_bias(layers[-2][1], len(anchors))
+    finally:
         if remaining > 0:
             logging.warning('%d bytes remaining' % remaining)
-    layers[-1][1] = transpose_weight(layers[-1][1], len(anchors))
-    layers[-2][1] = transpose_bias(layers[-2][1], len(anchors))
-    state_dict = dict(layers)
-    dnn.load_state_dict(state_dict)
-    if args.delete:
-        logging.warning('delete model directory: ' + model_dir)
-        shutil.rmtree(model_dir, ignore_errors=True)
-    saver = utils.train.Saver(model_dir, config.getint('save', 'keep'))
-    saver(dict(dnn=state_dict), 0, 0)
+        state_dict.update(dict(layers))
+        dnn.load_state_dict(state_dict)
+        if args.delete:
+            logging.warning('delete model directory: ' + model_dir)
+            shutil.rmtree(model_dir, ignore_errors=True)
+        saver = utils.train.Saver(model_dir, config.getint('save', 'keep'))
+        saver(dict(dnn=state_dict), 0, 0)
 
 
 def make_args():
