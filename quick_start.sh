2b130d4d46bbdd75c7dc26cb0526e95ea0ef5e1c
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2018-01-19 20:39:23 +0800

    使用aria2下载，断点续传可以防止一些不必要的错误

--- quick_start.sh
+++ quick_start.sh
@@ -7,7 +7,7 @@ http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
 ROOT=~/data
 for LINK in $LINKS
 do
-	wget $LINK -nc -P $ROOT
+	aria2c --auto-file-renaming=false -d $ROOT $LINK
 	tar -kxvf $ROOT/$(basename $LINK) -C $ROOT
 done
 
@@ -23,7 +23,7 @@ http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 ROOT=~/data/coco
 for LINK in $LINKS
 do
-	wget $LINK -nc -P $ROOT
+	aria2c --auto-file-renaming=false -d $ROOT $LINK
 	unzip -n $ROOT/$(basename $LINK) -d $ROOT
 done
 rm $ROOT/val2014/COCO_val2014_000000320612.jpg
@@ -33,6 +33,8 @@ python3 cache.py -m cache/datasets=cache.voc.cache cache/name=cache_voc cache/ca
 echo test models with 20 classes
 python3 cache.py -m cache/datasets='cache.voc.cache cache.coco.cache' cache/name=cache_20 cache/category=config/category/20
 
+ROOT=~/model/darknet
+
 MODELS="
 yolo-voc
 tiny-yolo-voc
@@ -40,7 +42,7 @@ tiny-yolo-voc
 
 for MODEL in $MODELS
 do
-	wget http://pjreddie.com/media/files/$MODEL.weights -nc -P ~/model/darknet
+	aria2c --auto-file-renaming=false -d $ROOT http://pjreddie.com/media/files/$MODEL.weights
 	python3 convert_darknet_torch.py ~/model/darknet/$MODEL.weights -c config.ini config/darknet/$MODEL.ini -d
 	python3 eval.py -c config.ini config/darknet/$MODEL.ini -m cache/name=cache_20
 	python3 detect.py -c config.ini config/darknet/$MODEL.ini -i image.jpg --pause
@@ -55,15 +57,15 @@ yolo
 
 for MODEL in $MODELS
 do
-	wget http://pjreddie.com/media/files/$MODEL.weights -nc -P ~/model/darknet
+	aria2c --auto-file-renaming=false -d $ROOT http://pjreddie.com/media/files/$MODEL.weights
 	python3 convert_darknet_torch.py ~/model/darknet/$MODEL.weights -c config.ini config/darknet/$MODEL.ini -d
 	python3 eval.py -c config.ini config/darknet/$MODEL.ini -m cache/name=cache_80
 	python3 detect.py -c config.ini config/darknet/$MODEL.ini -i image.jpg --pause
 done
 
-wget http://pjreddie.com/media/files/darknet19_448.conv.23 -nc -P ~/model/darknet
+aria2c --auto-file-renaming=false -d $ROOT http://pjreddie.com/media/files/darknet19_448.conv.23
+python3 convert_darknet_torch.py ~/model/darknet/darknet19_448.conv.23 -m model/name=$MODEL_NAME model/dnn=$MODEL -d --copy ~/model/darknet/darknet19_448.conv.23.pth
 
 export CACHE_NAME=cache_voc MODEL_NAME=model_voc MODEL=model.yolo2.Darknet
-python3 convert_darknet_torch.py ~/model/darknet/darknet19_448.conv.23 -m model/name=$MODEL_NAME model/dnn=$MODEL -d
-python3 train.py -b 64 -lr 1e-3 -e 160 -m cache/name=$CACHE_NAME model/name=$MODEL_NAME model/dnn=$MODEL train/optimizer='lambda params, lr: torch.optim.SGD(params, lr, weight_decay=5e-4, momentum=0.9)' train/scheduler='lambda optimizer: torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 90], gamma=0.1)'
+python3 train.py -b 64 -lr 1e-3 -e 160 -m cache/name=$CACHE_NAME model/name=$MODEL_NAME model/dnn=$MODEL train/optimizer='lambda params, lr: torch.optim.SGD(params, lr, weight_decay=5e-4, momentum=0.9)' train/scheduler='lambda optimizer: torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 90], gamma=0.1)' -f ~/model/darknet/darknet19_448.conv.23.pth -d
 python3 eval.py -m cache/name=$CACHE_NAME model/name=$MODEL_NAME model/dnn=$MODEL
